e20c86fc7e4b0e182c45f70b775ea7d4122db825
Aleksandr Shereshkov <aleksandr.shereshkov@gmail.com>
2022-08-01 01:30:23 +0300

    add dz

---
+++ src/components/App/App.jsx
@@ -0,0 +1,74 @@
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+import Message from './Message/Message';
+import s from './App.module.css';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addContact = ({ name, number }) => {
+    const { contacts } = this.state;
+    const newContact = { id: nanoid(), name, number };
+
+    contacts.some(contact => contact.name === name)
+      ? Report.warning(`${name}`, 'This user is already in the contact list.', 'OK')
+      : this.setState(({ contacts }) => ({
+          contacts: [newContact, ...contacts],
+        }));
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  changeFilter = e => {
+    const input = e.currentTarget;
+    this.setState({ filter: input.value });
+  };
+
+  filtredContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(({ name }) => name.toLowerCase().includes(normalizedFilter));
+  };
+
+  render() {
+    const { filter } = this.state;
+    const addContact = this.addContact;
+    const changeFilter = this.changeFilter;
+    const filtredContacts = this.filtredContacts();
+    const deleteContact = this.deleteContact;
+    const length = this.state.contacts.length;
+
+    return (
+      <div className={s.container}>
+        <h1 className={s.title}>Phone book</h1>
+        <ContactForm onSubmit={addContact} />
+
+        <h2 className={s.subtitle}>Contacts</h2>
+        <Filter filter={filter} changeFilter={changeFilter} />
+        {length > 0 ? (
+          <ContactList contacts={filtredContacts} onDeleteContact={deleteContact} />
+        ) : (
+          <Message text="Contact list is empty." />
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
